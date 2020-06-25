17087af9644c378ce170b4a4e4893103af1cff58
Michael Chen <michaelchengaming@gmail.com>
2020-06-25 02:19:54 -0700

    fetch different set of markers dependingo n zoom, country level vs state level etc

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,7 +3,7 @@ import ReactMapGl, { Marker } from "react-map-gl";
 import corona from "../assets/coronaLogo.svg";
 
 const Map = () => {
-  // sets initial location on map
+  // initial map configs
   const [viewport, setViewport] = useState({
     width: "100vw",
     height: "75vh",
@@ -13,19 +13,41 @@ const Map = () => {
   });
 
   const [apiError, setApiError] = useState(false);
-  const [countries, setCountries] = useState([]);
+  const [markers, setMarkers] = useState([]);
+  const [fetched, setFetched] = useState({
+    countries: false,
+    provinces: false,
+    cities: false,
+  });
+
+  const fetchCases = async (level = "countries") => {
+    const res = await fetch(`https://www.trackcorona.live/api/${level}`);
+    const data = await res.json();
+    console.log(data.data);
+    if (!data.code === 200) setApiError(true);
+    setMarkers(data.data);
+  };
 
   // fetch from api on app load
   useEffect(() => {
-    const fetchCountries = async () => {
-      const res = await fetch("https://www.trackcorona.live/api/countries");
-      const data = await res.json();
-      console.log(data.data);
-      if (!data.code === 200) setApiError(true);
-      setCountries(data.data);
-    };
-    fetchCountries();
+    fetchCases();
+    setFetched({ countries: true });
   }, []);
+
+  // fetch different end point on zoom change
+  useEffect(() => {
+    if (viewport.zoom >= 3 && viewport.zoom <= 7 && !fetched.provinces) {
+      fetchCases("provinces");
+      setFetched({ provinces: true, countries: false });
+    } else if (viewport.zoom < 3 && !fetched.countries) {
+      fetchCases();
+      setFetched({ countries: true, provinces: false });
+    }
+    // else if (viewport.zoom > 8 && !fetched.cities) {
+    //   fetchCases("cities");
+    //   setFetched({ cities: true });
+    // }
+  }, [viewport.zoom]);
 
   const btnSize = () => {
     if (viewport.zoom < 3) {
@@ -42,10 +64,13 @@ const Map = () => {
       {...viewport}
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
       onViewportChange={(viewport) => setViewport(viewport)}
-      mapStyle="mapbox://styles/proteinbro/ckbr18uqs3im51ioj2qzlgx7u"
+      // mapStyle="mapbox://styles/proteinbro/ckbr18uqs3im51ioj2qzlgx7u"
+      // mapStyle="mapbox://styles/proteinbro/ckbuim9uo0iqh1ipdx8q6v199"
+      mapStyle="mapbox://styles/proteinbro/ckbuivjuv0ixl1ip4uknqcbof"
     >
+      <span className="zoom">Zoom: {viewport.zoom}</span>
       {!apiError
-        ? countries.map((country) => (
+        ? markers.map((country) => (
             <Marker
               key={country.location + country.country_code}
               latitude={country.latitude}
