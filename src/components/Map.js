684bc8f8bceafa39e910bf5ac5814143833fd8f2
Michael Chen <michaelchengaming@gmail.com>
2020-06-25 00:37:31 -0700

    change button size to fit corona logo better, only render markers if no errors on fetch

--- src/components/Map.js
+++ src/components/Map.js
@@ -5,14 +5,14 @@ import corona from "../assets/coronaLogo.svg";
 const Map = () => {
   // sets initial location on map
   const [viewport, setViewport] = useState({
-    width: "80vh",
+    width: "90vw",
     height: "80vh",
-    latitude: 37.7577,
-    longitude: -122.4376,
+    // latitude: 37.7577,
+    // longitude: -122.4376,
     zoom: 1,
   });
 
-  const [fetchError, setFetchError] = useState(false);
+  const [apiError, setApiError] = useState(false);
   const [countries, setCountries] = useState([]);
 
   // fetch from api on app load
@@ -21,7 +21,7 @@ const Map = () => {
       const res = await fetch("https://www.trackcorona.live/api/countries");
       const data = await res.json();
       console.log(data.data);
-      if (!data.code === 200) setFetchError(true);
+      if (!data.code === 200) setApiError(true);
       setCountries(data.data);
     };
     fetchCountries();
@@ -34,20 +34,19 @@ const Map = () => {
       onViewportChange={(viewport) => setViewport(viewport)}
       mapStyle="mapbox://styles/proteinbro/ckbr18uqs3im51ioj2qzlgx7u"
     >
-      {countries.map((country) => {
-        // console.log(country);
-        return (
-          <Marker
-            key={country.location + country.country_code}
-            latitude={country.latitude}
-            longitude={country.longitude}
-          >
-            <button className="marker">
-              <img src={corona} alt="corona logo" />
-            </button>
-          </Marker>
-        );
-      })}
+      {!apiError
+        ? countries.map((country) => (
+            <Marker
+              key={country.location + country.country_code}
+              latitude={country.latitude}
+              longitude={country.longitude}
+            >
+              <button className="marker">
+                <img src={corona} alt="corona logo" />
+              </button>
+            </Marker>
+          ))
+        : null}
     </ReactMapGl>
   );
 };
