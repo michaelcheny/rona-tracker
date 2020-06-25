9175543069b23da90b5f8818ad4243d8b02efc12
Michael Chen <michaelchengaming@gmail.com>
2020-06-25 02:55:55 -0700

    add navbar and styling to it, fake link and logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.scss";
 import Map from "./components/Map";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div className="main">
+      <Navbar />
       <h1>The Corona Tracker 1.0</h1>
       <Map />
       <p style={{ fontSize: "8px" }}>Icons by svgrepo.com</p>
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="logo">LOGO</div>
+      <ul>
+        <li>thing</li>
+        <li>thing</li>
+        <li>thing</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
